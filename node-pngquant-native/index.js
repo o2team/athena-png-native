70a9e60a05cd18327df5bd6ac0b3205f832b1b44
luckyadam <weitaozsh@gmail.com>
2016-01-18 16:12:28 +0800

    add 5.x

--- node-pngquant-native/index.js
+++ node-pngquant-native/index.js
@@ -24,7 +24,7 @@ Pngquant.prototype = {
                                 if (l < 0) {
                                     l = 0;
                                 }
-                                
+
                                 if (h > 100) {
                                     h = 100;
                                 }
@@ -55,8 +55,12 @@ Pngquant.prototype = {
             this.option(opt);
         }
 
-        var handle = new _handle.Pngquant();
-        out = handle.compress(buffer, this.params, function nope(){});
+        if (process.versions.node >= '4.0.0') {
+            out = (new _handle(buffer, this.params)).compress();
+        } else {
+            var handle = new _handle.Pngquant();
+            out = handle.compress(buffer, this.params, function nope(){});
+        }
 
         return out;
     }
